aec3a4e09facf3486ad050f9a61b24a78257916f
Tab <1023106595@qq.com>
2019-02-04 18:22:22 +0800

    忽略logs和.vscode文件夹的提交

--- middleware/logger/log.js
+++ middleware/logger/log.js
@@ -7,7 +7,7 @@ module.exports = () => {
         appenders: {
             cheese: {
                 type: 'dateFile', // 日志类型 
-                filename: `log/task`, // 输出的文件名
+                filename: `logs/task`, // 输出的文件名
                 pattern: '-yyyy-MM-dd.log', // 文件名增加后缀
                 alwaysIncludePattern: true // 是否总是有后缀名
             }
