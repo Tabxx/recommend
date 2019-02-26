c80605e5382a270a15b13179600027df4654b8bb
Tab <1023106595@qq.com>
2019-02-26 20:54:35 +0800

    add admin

---
+++ views/admin/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <nav-tree></nav-tree>
+        </el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+
+  </div>
+</template>
+
+<script>
+import Nav from "@/components/common/Nav.vue";
+import Headbox from "@/components/common/Head.vue";
+export default {
+  name: "app",
+  components: {
+    NavTree: Nav
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
