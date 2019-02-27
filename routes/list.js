74629eba5539781a07b2e26ce8e4a9778c98a6b7
yuxinxin <yuxinxin@myhexin.com>
2019-02-27 19:36:31 +0800

    方案列表接口排序逻辑

--- routes/list.js
+++ routes/list.js
@@ -1,31 +1,31 @@
 const router = require('koa-router')()
-const sql = require('../utils/sql');
-const query = require('../utils/query');
-const utils = require('../utils/utils');
+const sql = require('../utils/sql')
+const query = require('../utils/query')
+const utils = require('../utils/utils')
 
 // 获取硬盘列表
 router.post('/add', async (ctx, next) => {
-    let param = ctx.request.body;
+    let param = ctx.request.body
 
     // 用户信息
     if (!param.userid) {
         ctx.body = {
             code: 1,
-            msg: "用户信息不存在",
+            msg: '用户信息不存在',
             result: null
         }
-        return;
+        return
     }
     // 添加当前时间
-    param.time = parseInt(Date.now() / 1000);
+    param.time = parseInt(Date.now() / 1000)
     // 获取参数信息
-    let key = Object.getOwnPropertyNames(param);
-    let val = Object.values(param);
+    let key = Object.getOwnPropertyNames(param)
+    let val = Object.values(param)
 
     // 拼接sql
-    let add_sql = sql.INERT_TABLE('list', key.join(','), val.map(item => `'${item}'`).join(','));
+    let add_sql = sql.INERT_TABLE('list', key.join(','), val.map(item => `'${item}'`).join(','))
     // 插入数据库
-    let result = await query.query(add_sql);
+    let result = await query.query(add_sql)
 
     if (result.affectedRows == 1) {
         ctx.body = {
@@ -44,12 +44,29 @@ router.post('/add', async (ctx, next) => {
 
 // 获取方案列表
 router.get('/getlist', async (ctx, next) => {
-    let id = ctx.request.query.id;
-    let list = [];
-    if (!id) {
-        list = await query.query(sql.QUERY_TABLE('list', '*'));
+    let {
+        id,
+        sortby = 0,
+        type
+    } = ctx.request.query
+    let list = []
+    // 排序规则
+    let sortRule = {
+        1: 'time',
+        2: 'clicks',
+        3: 'total'
+    }
+
+    // 兼容type为字符串
+    type = Number(type);
+    if ([1, 2, 3].includes(type)) {
+        // TODO:用户信息未返回，后期连表查询
+        let query_sql = `SELECT * from list ORDER BY ${sortRule[type]} ${sortby == 1 ? 'ASC' : 'DESC'} limit 10`
+        list = await query.query(query_sql)
+    } else if (!id) {
+        list = await query.query(sql.QUERY_TABLE('list', '*'))
     } else {
-        list = await query.query(sql.QUERY_TABLE('list', '*', `id in (${id})`));
+        list = await query.query(sql.QUERY_TABLE('list', '*', `id in (${id})`))
     }
 
     ctx.body = {
@@ -63,10 +80,10 @@ router.get('/getlist', async (ctx, next) => {
  * 方案点击量添加
  */
 router.get('/addClick', async (ctx, next) => {
-    let list_id = ctx.query.id;
+    let list_id = ctx.query.id
     if (list_id) {
-        let add_sql = `update list set clicks=clicks+1 where id=${list_id}`;
-        await query.query(add_sql);
+        let add_sql = `update list set clicks=clicks+1 where id=${list_id}`
+        await query.query(add_sql)
     }
 })
-module.exports = router;
+module.exports = router
