619db060e330c2d9259b37ce0df91bc50d1405b7
yuxinxin <yuxinxin@myhexin.com>
2019-03-06 20:37:30 +0800

    取消打包生成的map文件

--- views/admin/vue.config.js
+++ views/admin/vue.config.js
@@ -2,14 +2,14 @@ module.exports = {
     baseUrl: './',
     outputDir: 'html', // 输出目录
     devServer: {
-         proxy: {
-             '/': {
-                 target: 'http://localhost:3000',
-                 changeOrigin: true,
-                 ws: false
-             }
-         }
-     },
+        proxy: {
+            '/': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+                ws: false
+            }
+        }
+    },
     filenameHashing: false, // 打包是否生成哈希值
-    productionSourceMap: true, //
+    productionSourceMap: false, //
 }
